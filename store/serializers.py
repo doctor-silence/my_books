ce46e36cd98fcc007fa7f5bc5cb419373249855b
doctor-silence <vladimirkrizhanovski@gmail.com>
2022-08-29 12:39:40 +0300

    add_serializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,9 +1,35 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
-from store.models import Book
+from store.models import Book, UserBookRelation
 
 
-class BookSerializer(ModelSerializer):
+class BookReaderSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name')
+
+class BooksSerializer(ModelSerializer):
+    likes_count = serializers.SerializerMethodField()
+    annotated_likes = serializers.IntegerField(read_only=True)
+    rating = serializers.DecimalField(max_digits=3, decimal_places=2, read_only=True)
+    owner_name = serializers.CharField(source='owner.username', default='',
+                                       read_only=True)
+    readers = BookReaderSerializer(many=True, read_only=True)
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ('id', 'name', 'price', 'author_name', 'likes_count',
+                  'rating', 'owner_name', 'readers', 'annotated_likes')
+
+
+    def get_likes_count(selfs, instance):
+        return UserBookRelation.objects.filter(book=instance, like=True).count()
+
+
+
+class UserBookRelationSerializer(ModelSerializer):
+    class Meta:
+        model = UserBookRelation
+        fields = ('book', 'like', 'in_bookmarks', 'rate')
+
